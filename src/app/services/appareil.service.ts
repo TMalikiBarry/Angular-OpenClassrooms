236d4ef470c94c650460eff4e35ed8ee61bf7b85
MalikiHacker <malikihacker@gmail.com>
2022-11-07 15:12:12 +0000

    Ajout de données

--- src/app/services/appareil.service.ts
+++ src/app/services/appareil.service.ts
@@ -1,7 +1,7 @@
-import { Subject } from 'rxjs';
+import {Subject} from 'rxjs';
 
 export class AppareilService {
-  
+
   appareilsSubject = new Subject<any[]>();
   private appareils = [
     {
@@ -23,57 +23,70 @@ export class AppareilService {
       id: 4,
       name: 'Portable',
       status: 'allumé'
-    }
-];
+    },
+    {
+      id: 5,
+      name: 'Micro-onde',
+      status: 'éteint'
+    },
+    {
+      id: 6,
+      name: 'Tesla',
+      status: 'éteint'
+    },
+    {
+      id: 7,
+      name: 'écouteurs bluetooth',
+      status: 'allumé'
+    },
+  ];
 
-getAppareilById(id: number): any {
-  const appareil = this.appareils.find(
-    (s) => {
-      return s.id === id;
-    }
-  );
-  return appareil;
-}
-
-emitAppareilSubject() {
-  this.appareilsSubject.next(this.appareils.slice());
-}
-
-switchOnAll() {
-  for(let appareil of this.appareils) {
-    appareil.status = 'allumé';
+  getAppareilById(id: number): any {
+    return this.appareils.find(
+      (s) => {
+        return s.id === id;
+      }
+    );
   }
-  this.emitAppareilSubject();
-}
 
-switchOffAll() {
-  for(let appareil of this.appareils) {
-    appareil.status = 'éteint';
+  emitAppareilSubject() {
+    this.appareilsSubject.next(this.appareils.slice());
+  }
+
+  switchOnAll() {
+    for (let appareil of this.appareils) {
+      appareil.status = 'allumé';
+    }
+    this.emitAppareilSubject();
+  }
+
+  switchOffAll() {
+    for (let appareil of this.appareils) {
+      appareil.status = 'éteint';
+    }
+    this.emitAppareilSubject();
+  }
+
+  switchOnOne(i: number) {
+    this.appareils[i].status = 'allumé';
+    this.emitAppareilSubject();
+  }
+
+  switchOffOne(i: number) {
+    this.appareils[i].status = 'éteint';
+    this.emitAppareilSubject();
+  }
+
+  addAppareil(name: string, status: string) {
+    const appareilObject = {
+      id: 0,
+      name: '',
+      status: ''
+    };
+    appareilObject.name = name;
+    appareilObject.status = status;
+    appareilObject.id = this.appareils[(this.appareils.length - 1)].id + 1;
+    this.appareils.push(appareilObject);
     this.emitAppareilSubject();
   }
 }
-
-switchOnOne(i: number) {
-  this.appareils[i].status = 'allumé';
-  this.emitAppareilSubject();
-}
-
-switchOffOne(i: number) {
-  this.appareils[i].status = 'éteint';
-  this.emitAppareilSubject();
-}
-
-addAppareil(name: string, status: string) {
-  const appareilObject = {
-    id: 0,
-    name: '',
-    status: ''
-  };
-  appareilObject.name = name;
-  appareilObject.status = status;
-  appareilObject.id = this.appareils[(this.appareils.length - 1)].id + 1;
-  this.appareils.push(appareilObject);
-  this.emitAppareilSubject();
-}
-}
-  
